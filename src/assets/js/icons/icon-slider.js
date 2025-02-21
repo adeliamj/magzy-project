006b57381dde47ae63d1a0a033a4cc9e45200a71
adeliamj <adelia.miftakul.2205356@students.um.ac.id>
2025-02-21 14:58:01 +0700

    fix(event): responsive page on mobile

--- src/assets/js/icons/icon-slider.js
+++ src/assets/js/icons/icon-slider.js
@@ -44,7 +44,7 @@ class IconSlider extends HTMLElement {
 
     render() {
         this.innerHTML = `
-            <svg width="428" height="3" viewBox="0 0 428 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="100%" height="100%" viewBox="0 0 428 3" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect width="428" height="3" fill="#8B8B8B"/>
                 <rect width="0%" height="3" fill="#E36B45" class="active"/>
             </svg>
