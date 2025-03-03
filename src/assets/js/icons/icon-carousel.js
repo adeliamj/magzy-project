7477a38ec647630434db7eee56ded4fcd2518ee3
adeliamj <adelia.miftakul.2205356@students.um.ac.id>
2025-03-03 12:19:02 +0700

    fix(event): Updated icon-carousel.js, _event.scss, and index.html to reflect changes in icon carousel component

--- src/assets/js/icons/icon-carousel.js
+++ src/assets/js/icons/icon-carousel.js
@@ -9,9 +9,11 @@ class IconCarousel extends HTMLElement {
 
     render() {
         this.outerHTML = `
-      <svg width="100%" height="100%" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.5 5.3577V6.8577H16.9425L4.5 19.3002L5.5575 20.3577L18 7.9152V17.3577H19.5V5.3577L7.5 5.3577Z" fill="currentColor"/>
+      <svg width="100%" height="100%" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="31" height="31" rx="15.5" stroke="#E4E3E2"/>
+<path d="M14.8284 16.0007L19.7782 20.9504L18.364 22.3646L12 16.0007L18.364 9.63672L19.7782 11.0509L14.8284 16.0007Z" fill="#E4E3E2"/>
 </svg>
+
         `;
     }
 }
